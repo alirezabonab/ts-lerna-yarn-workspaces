aaeac076595bdce7fcbdff2c89617d4364ded15b
Alireza Bonab <alireza.bonab@enklare.se>
2021-12-29 00:48:16 +0100

    add divide feature

--- code/libs/common/src/index.ts
+++ code/libs/common/src/index.ts
@@ -9,3 +9,7 @@ export function multiply(a: number, b: number): number {
 export function subtract(a: number, b: number): number {
   return a - b;
 }
+
+export function divide(a: number, b: number): number {
+  return a / b;
+}
